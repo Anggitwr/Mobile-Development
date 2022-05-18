b26a3c918dd32e784765b1b280b51dee3bb50ee2
anggit wahyu <anggitwahyu23@gmail.com>
2022-05-19 03:44:34 +0700

    add activity sign up

--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/signup/SignupActivity.kt
@@ -2,10 +2,16 @@ package com.c22ps129.mobiledevelopment.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.c22ps129.mobiledevelopment.R
+import com.c22ps129.mobiledevelopment.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
