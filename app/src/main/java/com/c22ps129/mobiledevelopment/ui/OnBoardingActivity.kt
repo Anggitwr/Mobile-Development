d8887e34855eb0075232c06334852f8708f2bd44
haqi206 <19523206@students.uii.ac.id>
2022-05-18 09:03:46 +0700

    Commit OnBoardingActivity

---
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps129.mobiledevelopment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps129.mobiledevelopment.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
