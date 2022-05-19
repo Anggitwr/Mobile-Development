e4da6dffd3f9fa5615c919567b74b956a63725dd
anggit wahyu <anggitwahyu23@gmail.com>
2022-05-19 13:38:28 +0700

    update activity main xml

--- app/src/main/java/com/c22ps129/mobiledevelopment/MainActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/MainActivity.kt
@@ -2,10 +2,15 @@ package com.c22ps129.mobiledevelopment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.c22ps129.mobiledevelopment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
