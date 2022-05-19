058018b0272116bf760817ad8b76c777df768a9c
haqi206 <19523206@students.uii.ac.id>
2022-05-19 10:03:41 +0700

    add profileactivity

---
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.c22ps129.mobiledevelopment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c22ps129.mobiledevelopment.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
