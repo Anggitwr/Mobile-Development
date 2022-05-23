fd92ed67db821c8c65bdcc064697cc0841db0474
anggit wahyu <anggitwahyu23@gmail.com>
2022-05-23 18:49:20 +0700

    update main activity

--- app/src/main/java/com/c22ps129/mobiledevelopment/MainActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.c22ps129.mobiledevelopment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.c22ps129.mobiledevelopment.databinding.ActivityMainBinding
+import com.c22ps129.mobiledevelopment.ui.ProfileActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +21,38 @@ class MainActivity : AppCompatActivity() {
 
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+//        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+//
+//        val navController = findNavController(R.id.bottom_nav)
+//
+//        val appBarConfiguration = AppBarConfiguration.Builder(
+//            R.id.nav_profile
+//        ).build()
+//
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+    }
+
+
+    private fun action(){
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.profile ->{
+                startActivity(Intent(this, ProfileActivity::class.java))
+                true
+            }
+            else ->{return super.onOptionsItemSelected(item)}
+        }
     }
 }
--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/login/LoginActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.c22ps129.mobiledevelopment.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.c22ps129.mobiledevelopment.databinding.ActivityLoginBinding
+import com.c22ps129.mobiledevelopment.ui.ProfileActivity
+import com.c22ps129.mobiledevelopment.ui.signup.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,5 +17,13 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        action()
+    }
+    private fun action(){
+        binding.tvSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
