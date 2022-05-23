2b49180f4cc16d33b585fda7fb8b34a06f8c691a
haqi206 <19523206@students.uii.ac.id>
2022-05-23 17:11:34 +0700

    setupview on OnBoardingActivity.kt

--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/OnBoardingActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/OnBoardingActivity.kt
@@ -1,12 +1,38 @@
 package com.c22ps129.mobiledevelopment.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.c22ps129.mobiledevelopment.R
+import com.c22ps129.mobiledevelopment.databinding.ActivityMainBinding
+import com.c22ps129.mobiledevelopment.databinding.ActivityOnBoardingBinding
+import com.c22ps129.mobiledevelopment.ui.customview.EditTextPassword
 
 class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnBoardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+
+        binding= ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
