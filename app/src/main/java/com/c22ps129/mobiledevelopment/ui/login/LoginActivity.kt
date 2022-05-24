2f3824fd552889e2970521ce474957eae52e7671
haqi206 <19523206@students.uii.ac.id>
2022-05-24 07:14:48 +0700

    commit added setupView

--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/login/LoginActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/login/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.c22ps129.mobiledevelopment.ui.login
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.c22ps129.mobiledevelopment.databinding.ActivityLoginBinding
 import com.c22ps129.mobiledevelopment.ui.ProfileActivity
 import com.c22ps129.mobiledevelopment.ui.signup.SignupActivity
@@ -10,20 +13,34 @@ import com.c22ps129.mobiledevelopment.ui.signup.SignupActivity
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
-
+        setupView()
         action()
     }
+
     private fun action(){
         binding.tvSignup.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/signup/SignupActivity.kt
@@ -1,19 +1,36 @@
 package com.c22ps129.mobiledevelopment.ui.signup
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.c22ps129.mobiledevelopment.R
 import com.c22ps129.mobiledevelopment.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
         supportActionBar?.hide()
     }
 }
