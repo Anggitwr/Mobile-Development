0fa2e209504c620ae4cc9026a762857feed3b374
haqi206 <19523206@students.uii.ac.id>
2022-05-24 07:59:21 +0700

    bottom navigation test

--- app/src/main/java/com/c22ps129/mobiledevelopment/ui/ProfileActivity.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/ui/ProfileActivity.kt
@@ -2,11 +2,20 @@ package com.c22ps129.mobiledevelopment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.c22ps129.mobiledevelopment.R
+import com.c22ps129.mobiledevelopment.databinding.ActivityProfileBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
