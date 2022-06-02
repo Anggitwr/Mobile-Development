d60be7c3896e300a15c1457e0861c57abf2cd240
haqi206 <19523206@students.uii.ac.id>
2022-06-02 18:12:20 +0700

    update SignupResponse.kt

--- app/src/main/java/com/c22ps129/mobiledevelopment/response/SignupResponse.kt
+++ app/src/main/java/com/c22ps129/mobiledevelopment/response/SignupResponse.kt
@@ -6,7 +6,32 @@ data class SignupResponse (
     @field:SerializedName("success")
     val success: Boolean,
 
+    @field:SerializedName("data")
+    val data: DataResult
+)
+
+data class DataResult(
+    @field:SerializedName("fieldCount")
+    val fieldCount: Int,
+
+    @field:SerializedName("affectedRows")
+    val affectedRows: Int,
+
+    @field:SerializedName("insertId")
+    val insertId: Int,
+
+    @field:SerializedName("serverStatus")
+    val serverStatus: Int,
+
+    @field:SerializedName("warningCount")
+    val warningCount: Int,
+
     @field:SerializedName("message")
     val message: String,
 
+    @field:SerializedName("protocol41")
+    val protocol41: Boolean,
+
+    @field:SerializedName("changedRows")
+    val changedRows: Int
 )
